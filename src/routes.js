6240fd914518e295d22d63e864b739cc37ef4bb8
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-03-12 14:31:36 -0300

    feat: add view favorite movie list

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Favoritos from "./pages/Favoritos";
 import Erro from "./pages/Erro";
+
 import Header from "./components/Header";
 
 function RoutesApp() {
@@ -11,7 +13,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
-
+        <Route path="/favoritos" element={<Favoritos />} />
         <Route path="*" element={<Erro />} />
       </Routes>
     </BrowserRouter>
