d4e1d7e4c18ac18c78085822cf4a872e3410541a
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-03-11 21:01:25 -0300

    feat: add search movie trailer

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "./filme-info.css";
 import api from "../../services/api";
 
 function Filme() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -23,6 +24,8 @@ function Filme() {
         })
         .catch(() => {
           console.log("FILME NÃO ENCONTRADO");
+          navigate("/", { replace: true });
+          return;
         });
     }
 
@@ -31,7 +34,7 @@ function Filme() {
     return () => {
       console.log("COMPONENTE FOI DESMONTADO");
     };
-  }, []);
+  }, [navigate, id]);
 
   if (loading) {
     return (
@@ -57,7 +60,13 @@ function Filme() {
       <div className="area-buttons">
         <button>Salvar</button>
         <button>
-          <a href="#">Trailer</a>
+          <a
+            target="_blank"
+            rel="external"
+            href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
+          >
+            Trailer
+          </a>
         </button>
       </div>
     </div>
