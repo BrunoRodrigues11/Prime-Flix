da1823c1cafd039c9dd0ed1fb233bb9360571f44
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-03-13 22:34:49 -0300

    style(ui): change background gradient

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -34,7 +34,6 @@ function Home() {
         },
       });
 
-      // console.log(response.data.results.slice(0, 10));
       setFilmes(response.data.results.slice(0, 10));
       setLoading(false);
     }
