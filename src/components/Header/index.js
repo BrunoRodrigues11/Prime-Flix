dfcb60495443e1ebf744c96f4b23f7e396ec629d
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-03-18 23:50:12 -0300

    feat: icon bookmark change on favorite movies page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,50 @@
 import "./header.css";
-import { Link } from "react-router-dom";
-import { BsBookmark } from "react-icons/bs";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { BsBookmark, BsBookmarkFill } from "react-icons/bs";
 
 function Header() {
+  // Estado para controlar se a página atual é de favoritos
+  const [isFavoritesPage, setIsFavoritesPage] = useState(false);
+
+  // Use useLocation do react-router-dom para obter a localização atual
+  const location = useLocation();
+
+  // Verifica se a página atual é de favoritos
+  React.useEffect(() => {
+    setIsFavoritesPage(location.pathname === "/favoritos");
+  }, [location]);
+
   return (
     <header>
       <Link className="logo" to="/">
         Prime Flix
       </Link>
       <div className="menu">
-        <Link to="/" className="active">
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>
           Início
         </Link>
-        <Link to="/">Séries</Link>
-        <Link to="/">Filmes</Link>
+        <Link
+          to="/series"
+          className={location.pathname === "/series" ? "active" : ""}
+        >
+          Séries
+        </Link>
+        <Link
+          to="/filmes"
+          className={location.pathname === "/filmes" ? "active" : ""}
+        >
+          Filmes
+        </Link>
       </div>
       <div className="menu-opcoes">
+        {/* Renderiza o ícone de bookmark condicionalmente com base na página atual */}
         <Link className="favoritos" to="/favoritos">
-          <BsBookmark className="icone" />
+          {isFavoritesPage ? (
+            <BsBookmarkFill className="icone" />
+          ) : (
+            <BsBookmark className="icone" />
+          )}
         </Link>
         <div className="profile">
           <img
