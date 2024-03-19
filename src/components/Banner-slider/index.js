32e9321e7400344080a136d5ba4f89a1126637b7
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-03-18 23:47:18 -0300

    style(ui): hover effect on top movies list

--- src/components/Banner-slider/index.js
+++ src/components/Banner-slider/index.js
@@ -2,7 +2,6 @@ import "./banner-slider.css";
 import { Link } from "react-router-dom";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { FaRegPlayCircle } from "react-icons/fa";
 
 function BannerSlider(filmes_list) {
   return (
@@ -19,11 +18,13 @@ function BannerSlider(filmes_list) {
               <article className="banner-filmes-item">
                 <div className="banner-filmes-tittle">
                   <h1>{filme.title}</h1>
-                  <p>{filme.overview}</p>
+                  <p>
+                    {filme.overview.length > 150
+                      ? filme.overview.slice(0, 150) + "... Ler mais"
+                      : filme.overview}
+                  </p>
                   <Link to={`/filme/${filme.id}`}>
-                    <button>
-                      <FaRegPlayCircle /> Assistir
-                    </button>
+                    <button>Ir para o filme</button>
                   </Link>
                 </div>
                 <img
